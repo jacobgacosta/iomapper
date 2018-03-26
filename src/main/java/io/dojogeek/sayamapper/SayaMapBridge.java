11211bd3d1bb0a4704eb68f5d3126c695d480aba
Jacob G.Acosta <dosek17@gmail.com>
2018-03-25 22:10:58 -0600

    Remove variable instances for SayaMapBridge
    The instances are returned directly in the methods.

--- src/main/java/io/dojogeek/sayamapper/SayaMapBridge.java
+++ src/main/java/io/dojogeek/sayamapper/SayaMapBridge.java
@@ -5,17 +5,14 @@ package io.dojogeek.sayamapper;
  */
 public class SayaMapBridge<T, T2> implements BridgeMap<T, T2> {
 
-    private Mapper<T, T2> inner = new SayaMap<>();
-    private Mapper<T2, T> outer = new SayaMap<>();
-
     @Override
     public Mapper<T, T2> inner() {
-        return this.inner;
+        return new SayaMap<>();
     }
 
     @Override
     public Mapper<T2, T> outer() {
-        return this.outer;
+        return new SayaMap<>();
     }
 
 }
