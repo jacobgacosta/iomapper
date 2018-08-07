6c7f81685187c189d3d626609f84bd1121158407
Jacob G.Acosta <dosek17@gmail.com>
2018-08-07 16:41:07 -0500

    Add new Dtos and Models for testing

---
+++ src/test/java/io/dojogeek/dtos/UserDto.java
@@ -0,0 +1,27 @@
+package io.dojogeek.dtos;
+
+/**
+ * Created by norveo on 8/7/18.
+ */
+public class UserDto {
+
+    private String name;
+    private String job;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+}
---
+++ src/test/java/io/dojogeek/models/User.java
@@ -0,0 +1,27 @@
+package io.dojogeek.models;
+
+/**
+ * Created by norveo on 8/7/18.
+ */
+public class User {
+
+    private String name;
+    private String job;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+}
