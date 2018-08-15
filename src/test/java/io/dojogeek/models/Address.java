9a90495e3661d8c18d85c9924f62d65b963df581
Jacob G.Acosta <dosek17@gmail.com>
2018-08-14 19:50:56 -0500

    Add Dto and model for testing

---
+++ src/test/java/io/dojogeek/models/Address.java
@@ -0,0 +1,27 @@
+package io.dojogeek.models;
+
+/**
+ * Created by norveo on 8/13/18.
+ */
+public class Address {
+
+    private String state;
+    private String zip;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+}
