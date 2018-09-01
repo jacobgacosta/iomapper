055a433d2267776ed3263c912534e3f6f7480c1a
Jacob G.Acosta <dosek17@gmail.com>
2018-08-31 23:09:33 -0500

    Add a 'dismantle' method
    This method dismantle a function to split by name of function and
    arguments.

--- src/main/java/io/dojogeek/sayamapper/utils/MethodShredder.java
+++ src/main/java/io/dojogeek/sayamapper/utils/MethodShredder.java
@@ -1,7 +1,32 @@
 package io.dojogeek.sayamapper.utils;
 
-public class FunctionShredder {
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
-    public static
+public class MethodShredder {
+
+    public static SignatureMethod dismantle(String value) {
+        String methodName = value.substring(0, value.indexOf("("));
+
+        List<String> args = Arrays
+                .asList(value.substring(value.indexOf("(") + 1, value.indexOf(")")).split(","))
+                .stream()
+                .map(String::trim)
+                .collect(Collectors.toList());
+
+        SignatureMethod signatureMethod = new SignatureMethod();
+        signatureMethod.setName(methodName);
+        signatureMethod.setArgs(args);
+
+        return signatureMethod;
+    }
+
+    public static boolean isAMethod(String rootField) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+\\([a-zA-Z0-9]+(,\\s[a-zA-Z0-9]+)*\\)");
+
+        return pattern.matcher(rootField).find();
+    }
 
 }
