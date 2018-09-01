f7ce9783904d455861ab11abfc0d82ec0bb72dba
Jacob G.Acosta <dosek17@gmail.com>
2018-08-31 23:20:31 -0500

    Rename class

--- src/main/java/io/dojogeek/sayamapper/IgnorableFieldPathShredder.java
+++ src/main/java/io/dojogeek/sayamapper/IgnorableFieldPathShredder.java
@@ -1,5 +1,7 @@
 package io.dojogeek.sayamapper;
 
+import io.dojogeek.sayamapper.utils.Delimiters;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,28 +9,27 @@ import java.util.List;
 /**
  * Created by norveo on 8/2/18.
  */
-public class FieldPathShredder {
+public class IgnorableFieldPathShredder {
 
     private static final int SINGLE_FIELD = 1;
 
     private List<String> fields = new ArrayList<>();
 
-    public FieldPathShredder(String path) {
+    public IgnorableFieldPathShredder(String path) {
         Arrays
                 .asList(path.split(Delimiters.DOT_SEPARATOR))
                 .forEach(field -> fields.add(field));
     }
 
-    public RootField getRootField() {
-        return this.fields.isEmpty() ? new RootField("") : new RootField(this.fields.get(0));
+    public String getRootField() {
+        return fields.get(0);
     }
 
-    public boolean hasMoreFields() {
-        return this.fields.size() > SINGLE_FIELD;
+    public boolean hasNestedFields() {
+        return fields.size() > SINGLE_FIELD;
     }
 
     public void removeRootField() {
-        this.fields.remove(0);
+        fields.remove(0);
     }
-
 }
