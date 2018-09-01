42dfda964e61ce734413e5e7da5c36c431ba747b
Jacob G.Acosta <dosek17@gmail.com>
2018-09-01 18:12:32 -0500

    Create class to determine the mapping type

---
+++ src/main/java/io/dojogeek/sayamapper/Determiner.java
@@ -0,0 +1,28 @@
+package io.dojogeek.sayamapper;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by norveo on 8/7/18.
+ */
+public class Determiner {
+
+    public static boolean isNested(String value) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)+$");
+
+        return pattern.matcher(value).find();
+    }
+
+    public static boolean isSingle(String value) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+$");
+
+        return pattern.matcher(value).find();
+    }
+
+    public static boolean isFunction(String value) {
+        Pattern pattern = Pattern.compile("^([a-zA-Z0-9]+\\([a-zA-Z0-9]+(,\\s[a-zA-Z0-9]+)*\\))|[a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)+\\([a-zA-Z0-9]+(,\\s[a-zA-Z0-9]+)*\\)$");
+
+        return pattern.matcher(value).find();
+    }
+
+}
