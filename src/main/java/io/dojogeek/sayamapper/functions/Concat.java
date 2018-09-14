4d029956f04b739cda3c9dd0767614b657a2e53f
Jacob G.Acosta <dosek17@gmail.com>
2018-09-13 23:17:03 -0500

    Change 'concat' method implementation

--- src/main/java/io/dojogeek/sayamapper/functions/Concat.java
+++ src/main/java/io/dojogeek/sayamapper/functions/Concat.java
@@ -1,20 +1,48 @@
 package io.dojogeek.sayamapper.functions;
 
+import io.dojogeek.sayamapper.Determiner;
+
 import java.util.List;
+import java.util.logging.Logger;
 
 public class Concat implements Executable {
+
+    private final static Logger LOGGER = Logger.getLogger(Concat.class.getName());
 
     @Override
     public Object execute(Object... args) {
         String value = "";
+        String delimiter = "";
 
         List<Object> values = (List<Object>) args[0];
 
-        for (Object argument : values) {
-            value += argument;
+        String lastArgument = (String) values.get(values.size() - 1);
+
+        if (Determiner.isExtraArgument(lastArgument)) {
+            String delimiterArgument = this.getDelimiterFrom(lastArgument);
+
+            switch (delimiterArgument) {
+                case "s":
+                case "S":
+                    delimiter = " ";
+                    break;
+                default:
+                    delimiter = lastArgument;
+            }
+
+            values.remove(values.size() - 1);
         }
 
-        return value;
+        for (Object argument : values) {
+            value += argument + delimiter;
+        }
+
+        return value.trim();
     }
 
+    public String getDelimiterFrom(String delimiter) {
+        String[] split = delimiter.split("\\'");
+
+        return split[1];
+    }
 }
