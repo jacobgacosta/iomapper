9693ce90ae422edb5d5b892f91844c7733f301d6
Jacob G.Acosta <dosek17@gmail.com>
2018-10-12 15:09:14 -0500

    Create Classes  to test numerics mapping

---
+++ src/test/java/io/dojogeek/models/NumberModel.java
@@ -0,0 +1,72 @@
+package io.dojogeek.models;
+
+/**
+ * Created by norveo on 10/12/18.
+ */
+public class NumberModel {
+
+    private byte byteField;
+    private short shortField;
+    private int IntField;
+    private long longField;
+    private float floatField;
+    private double doubleField;
+    private boolean booleanField;
+
+    public byte getByteField() {
+        return byteField;
+    }
+
+    public void setByteField(byte byteField) {
+        this.byteField = byteField;
+    }
+
+    public short getShortField() {
+        return shortField;
+    }
+
+    public void setShortField(short shortField) {
+        this.shortField = shortField;
+    }
+
+    public int getIntField() {
+        return IntField;
+    }
+
+    public void setIntField(int intField) {
+        IntField = intField;
+    }
+
+    public long getLongField() {
+        return longField;
+    }
+
+    public void setLongField(long longField) {
+        this.longField = longField;
+    }
+
+    public float getFloatField() {
+        return floatField;
+    }
+
+    public void setFloatField(float floatField) {
+        this.floatField = floatField;
+    }
+
+    public double getDoubleField() {
+        return doubleField;
+    }
+
+    public void setDoubleField(double doubleField) {
+        this.doubleField = doubleField;
+    }
+
+    public boolean isBooleanField() {
+        return booleanField;
+    }
+
+    public void setBooleanField(boolean booleanField) {
+        this.booleanField = booleanField;
+    }
+
+}
