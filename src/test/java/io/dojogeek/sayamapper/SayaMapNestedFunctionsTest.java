8440f6dd4d542f67cde72442c89bf36f7fa8e562
Jacob G.Acosta <dosek17@gmail.com>
2018-10-17 14:37:54 -0500

    Add test case to check nested functions
    A function can receive a nested function as a parameter.

---
+++ src/test/java/io/dojogeek/sayamapper/SayaMapNestedFunctionsTest.java
@@ -0,0 +1,29 @@
+package io.dojogeek.sayamapper;
+
+import io.dojogeek.dtos.NumericDto;
+import io.dojogeek.models.NumericModel;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by norveo on 10/15/18.
+ */
+public class SayaMapNestedFunctionsTest {
+
+    private BridgeMap<NumericDto, NumericModel> map = new SayaMapBridge<>();
+
+    @Test
+    public void shouldSupportNestedFunctions() {
+        NumericModel numericModel = new NumericModel();
+        numericModel.setByteField((byte) 127);
+        numericModel.setShortField((short) 10000);
+
+        NumericDto numericDto = map.outer().from(numericModel).to(NumericDto.class).relate(customMapping ->
+                    customMapping.relate("toString(add(byteField, shortField))", "stringI")
+        ).build();
+
+        assertEquals("10127", numericDto.getStringI(), 0.001);
+    }
+
+}
