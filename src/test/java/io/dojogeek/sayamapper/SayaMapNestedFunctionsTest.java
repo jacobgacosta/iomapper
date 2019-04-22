ecee8ab438c92e94502d2039440a10f88d10ba70
Jacob Guzmán Acosta <dosek17@gmail.com>
2019-04-22 17:51:16 -0500

    Add test case
    The test case checks by non-existent methods.

--- src/test/java/io/dojogeek/sayamapper/SayaMapNestedFunctionsTest.java
+++ src/test/java/io/dojogeek/sayamapper/SayaMapNestedFunctionsTest.java
@@ -2,18 +2,40 @@ package io.dojogeek.sayamapper;
 
 import io.dojogeek.dtos.NumericDto;
 import io.dojogeek.models.NumericModel;
+import io.dojogeek.sayamapper.exceptions.NullValueException;
+import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Created by norveo on 10/15/18.
  */
 public class SayaMapNestedFunctionsTest {
 
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
     private BridgeMap<NumericDto, NumericModel> map = new SayaMapBridge<>();
 
     @Test
+    public void shouldThrowARuntimeExceptionForNonExistentFunction() {
+        expectedEx.expect(RuntimeException.class);
+        expectedEx.expectMessage("The 'nonexistentFunction' function doesn't exist.");
+
+        NumericModel numericModel = new NumericModel();
+        numericModel.setByteField((byte) 127);
+        numericModel.setShortField((short) 10000);
+
+        NumericDto numericDto = map.outer().from(numericModel).to(NumericDto.class).relate(customMapping ->
+                customMapping.relate("nonexistentFunction(byteField, shortField)", "stringI")
+        ).build();
+    }
+
+    @Ignore
     public void shouldSupportNestedFunctions() {
         NumericModel numericModel = new NumericModel();
         numericModel.setByteField((byte) 127);
