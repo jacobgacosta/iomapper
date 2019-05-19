d0ae28abd4e190542c96fe8537d45a2098272d50
Jacob Guzmán Acosta <jacob.gacosta@gmail.com>
2019-05-19 16:35:20 -0500

    Create classes for function parser implementation

---
+++ src/main/java/io/dojogeek/parser/Result.java
@@ -0,0 +1,18 @@
+package io.dojogeek.parser;
+
+/**
+ * Created by norveo on 11/15/18.
+ */
+public class Result {
+
+    private int value;
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
